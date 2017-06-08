ed293920401e75ec79d240dbb205dd4d13ca80b7
David Lukes <dafydd.lukes@gmail.com>
2017-06-08 12:00:54 +0200

    rewrite loadingsTable.js without jQuery

--- www/loadingsTable.js
+++ www/loadingsTable.js
@@ -8,25 +8,21 @@ function(row, data) {
     "#F0C9B5", "#E8D4C9", "#DDDDDD", "#CFD8E9", "#BFD2F3", "#AFC9FB", "#9EBDFE", "#8CAEFC",
     "#7B9DF7", "#698AEF", "#5876E1", "#4961D1", "#3B4CC0"
   ];
-  var colIdx = 0;
 
-  function colorize() {
-    var value = this.innerHTML.trim();
-    if (colIdx === 0) {
-      $(this).addClass("dt-right");
-    } else if (/^[\d\.\-]+$/.test(value)) {
-      value = parseFloat(value);
+  var cells = row.children;
+  cells[0].classList.add("dt-right");
+  for (var i = 0; i < data.length; i++) {
+    var value = data[i];
+    if (/^[\d\.\-]+$/.test(value)) {
       var round = value < 0 ? Math.floor : Math.ceil;
       var colorIdx = round((value + 1) / 2 * 20);
-      this.style.background = palette[colorIdx];
-      this.style.fontWeight = "bold";
+      var cell = cells[i];
+      cell.style.background = palette[colorIdx];
+      cell.style.fontWeight = "bold";
       if (Math.abs(value) > 0.6) {
-        this.style.color = "white";
+        cell.style.color = "white";
       }
-      this.innerHTML = value.toFixed(2);
+      cell.innerHTML = value.toFixed(2);
     }
-    colIdx++;
   }
-
-  $(row).find("td").each(colorize);
 }
