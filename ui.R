8d7bd1bccfcd2d1302fd4e34c85e5a681ae21ba5
David Lukes <dafydd.lukes@gmail.com>
2017-12-08 16:24:58 +0100

    Let well with controls scroll on overflow-y
    It's prettier. Also, if there's a figure, a re-render is triggered if a
    scroll-bar for the entire page appears, which in the case of the
    *Factors 2-dim* tab leads to canceling an active selection (if
    available), which removes selection info from the controls, which makes
    it shrink again → frustrating.

--- ui.R
+++ ui.R
@@ -9,62 +9,66 @@ function(request) {
 
     sidebarLayout(
       sidebarPanel(
+        id="controls-outer",
         width=3,
-        bookmarkButton(),
-        conditionalPanel(condition="input.tabsetPanel != 'MultiModelCmp'",
-          fileInput("rdata", "Upload your own data:", accept=".RData"),
-          selectInput("results", "Results:", choices=fresults, selected=fresults[1]),
-          conditionalPanel(condition="/^Factors/.test(input.tabsetPanel)",
-            selectInput("fx", "X axis:", choices=c()),
-            selectInput("fy", "Y axis:", choices=c())
+        div(
+          id="controls-inner",
+          bookmarkButton(),
+          conditionalPanel(condition="input.tabsetPanel != 'MultiModelCmp'",
+            fileInput("rdata", "Upload your own data:", accept=".RData"),
+            selectInput("results", "Results:", choices=fresults, selected=fresults[1]),
+            conditionalPanel(condition="/^Factors/.test(input.tabsetPanel)",
+              selectInput("fx", "X axis:", choices=c()),
+              selectInput("fy", "Y axis:", choices=c())
+            )
+          ),
+          conditionalPanel(condition="input.tabsetPanel == 'Factors 2-dim'",
+            fluidRow(
+              column(6, checkboxGroupInput("mode", "Mode:", choices=c())),
+              column(6, checkboxGroupInput("division", "Division:", choices=c()))
+            ),
+            h4("Selected chunk:"),
+            htmlOutput("click_info"),
+            textInput("cql", "CQL:", value='[lemma=".*"]'),
+            actionButton("search", "Search", onclick="kontextSearch()")
+          ),
+          conditionalPanel(condition="input.tabsetPanel == 'Factors Boxplots'",
+            div(
+              class="outer-range-wrapper",
+              sliderInput("perc", "Percentiles:", min=0, max=1, step=.05, value=c(.3, .7), width="100%")
+            )
+          ),
+          conditionalPanel(condition="input.tabsetPanel == 'Loadings Table'",
+            div(
+              class="outer-range-wrapper",
+              sliderInput("thresh", "Global thresholds:", min=-1, max=1, step=.05, value=c(-.3, .3), width="100%")
+            ),
+            checkboxGroupInput("showfactors", "Show factors:", choices=c(), width="100%")
+          ),
+          conditionalPanel(condition="input.tabsetPanel == 'GenreDiff'",
+            actionButton("subcorp1", "Specify Subcorpus 1"),
+            br(),
+            br(),
+            actionButton("subcorp2", "Specify Subcorpus 2")
+          ),
+          conditionalPanel(condition="input.tabsetPanel == 'ModelCmp'",
+            selectInput("cmp_results", "Compare with:", choices=fresults, selected=fresults[2])
+          ),
+          conditionalPanel(condition="input.tabsetPanel == 'MultiModelCmp'",
+            selectizeInput("model_seq", "Sequence of models:", fresults_names, selected=init_model_seq, multiple=TRUE),
+            h4("Focus on features:"),
+            selectizeInput("mmc_feat_set", "Individual:", feat2desc$Feature, multiple=TRUE),
+            selectizeInput("mmc_feats_from_mod", "Correlated with model:", choices=fresults_names, multiple=TRUE),
+            selectizeInput("mmc_feats_from_dim", "Dimension:", choices=1:9, multiple=TRUE),
+            div(
+              class="outer-range-wrapper",
+              sliderInput("mmc_feats_thresh", "Loading threshold:", min=-1, max=1, step=.05, value=c(-.3, .3), width="100%")
+            ),
+            h4("Details table (only when focus specified):"),
+            selectInput("mmc_dets_m1", "From model:", fresults_names, selected=init_model_seq[1]),
+            selectInput("mmc_dets_m2", "To model:", fresults_names, selected=init_model_seq[2]),
+            numericInput("mmc_dets_top", "Show only top loading products:", 20, min=0, step=1)
           )
-        ),
-        conditionalPanel(condition="input.tabsetPanel == 'Factors 2-dim'",
-          fluidRow(
-            column(6, checkboxGroupInput("mode", "Mode:", choices=c())),
-            column(6, checkboxGroupInput("division", "Division:", choices=c()))
-          ),
-          h4("Selected chunk:"),
-          htmlOutput("click_info"),
-          textInput("cql", "CQL:", value='[lemma=".*"]'),
-          actionButton("search", "Search", onclick="kontextSearch()")
-        ),
-        conditionalPanel(condition="input.tabsetPanel == 'Factors Boxplots'",
-          div(
-            class="outer-range-wrapper",
-            sliderInput("perc", "Percentiles:", min=0, max=1, step=.05, value=c(.3, .7), width="100%")
-          )
-        ),
-        conditionalPanel(condition="input.tabsetPanel == 'Loadings Table'",
-          div(
-            class="outer-range-wrapper",
-            sliderInput("thresh", "Global thresholds:", min=-1, max=1, step=.05, value=c(-.3, .3), width="100%")
-          ),
-          checkboxGroupInput("showfactors", "Show factors:", choices=c(), width="100%")
-        ),
-        conditionalPanel(condition="input.tabsetPanel == 'GenreDiff'",
-          actionButton("subcorp1", "Specify Subcorpus 1"),
-          br(),
-          br(),
-          actionButton("subcorp2", "Specify Subcorpus 2")
-        ),
-        conditionalPanel(condition="input.tabsetPanel == 'ModelCmp'",
-          selectInput("cmp_results", "Compare with:", choices=fresults, selected=fresults[2])
-        ),
-        conditionalPanel(condition="input.tabsetPanel == 'MultiModelCmp'",
-          selectizeInput("model_seq", "Sequence of models:", fresults_names, selected=init_model_seq, multiple=TRUE),
-          h4("Focus on features:"),
-          selectizeInput("mmc_feat_set", "Individual:", feat2desc$Feature, multiple=TRUE),
-          selectizeInput("mmc_feats_from_mod", "Correlated with model:", choices=fresults_names, multiple=TRUE),
-          selectizeInput("mmc_feats_from_dim", "Dimension:", choices=1:9, multiple=TRUE),
-          div(
-            class="outer-range-wrapper",
-            sliderInput("mmc_feats_thresh", "Loading threshold:", min=-1, max=1, step=.05, value=c(-.3, .3), width="100%")
-          ),
-          h4("Details table (only when focus specified):"),
-          selectInput("mmc_dets_m1", "From model:", fresults_names, selected=init_model_seq[1]),
-          selectInput("mmc_dets_m2", "To model:", fresults_names, selected=init_model_seq[2]),
-          numericInput("mmc_dets_top", "Show only top loading products:", 20, min=0, step=1)
         )
       ),
 
