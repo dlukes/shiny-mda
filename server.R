dlukes/shiny-mda

commit dbb801c9427c509b60e9ddf29ff42be0b17f7ba7

Make GenreDiff work with non-Koditex data
Up until now, GenreDiff only paid attention to the detailed Koditex
metadata loaded separately into globalMeta, which means that there was
no way to select subcorpora of chunks from extended datasets (e.g.
CPACT). This commit makes it possible by extracting these additional
chunks and their MODE and DIVISION values from the results, and adding
them to the metadata tibble.

server.R
@@ -300,7 +300,24 @@ function(input, output, session) {
 
   genreDiffVals <- reactiveValues(subcorp1=NULL, subcorp2=NULL)
 
-  subcorpModal <- function(subcorp, meta=globalMeta, selected=list(), force_include=FALSE) {
+  # globalMeta only contains Koditex chunks and their metadata; this adds any
+  # additional chunks and their MODE and DIVISION metadata found in the current
+  # results
+  globalAndResultsMeta <- reactive({
+    fdf <- data()$fdf
+    non_koditex_ids <- setdiff(rownames(fdf), globalMeta$id)
+    non_koditex_chunks <- fdf[non_koditex_ids, ] %>%
+      rownames_to_column("id") %>%
+      select(id, mode=MODE, division=DIVISION_ORIG)
+    # consider all additional chunks extracted from results as include=yes,
+    # otherwise the user would have to change this manually every time, because
+    # we want to keep force_include=TRUE for Koditex data
+    non_koditex_chunks$include <- "yes"
+    bind_rows(globalMeta, non_koditex_chunks) %>%
+      replace(is.na(.), "-NA-")
+  })
+
+  subcorpModal <- function(subcorp, meta, selected=list(), force_include=FALSE) {
     if (force_include) selected$include = "yes"
     categories <- colnames(meta) %>%
       purrr::discard(~ .x == "id") %>%
@@ -330,7 +347,7 @@ function(input, output, session) {
   }
 
   doSubcorpSelection <- function(subcorp) {
-    meta <- globalMeta
+    meta <- globalAndResultsMeta()
     chboxes <- grep(paste0("CheckboxSubcorp", subcorp), names(input), value=TRUE)
     selectedList <- list()
     for (chbox in chboxes) {
@@ -364,8 +381,8 @@ function(input, output, session) {
     removeModal()
   }
 
-  observeEvent(input$subcorp1, showModal(subcorpModal(1, force_include=TRUE)))
-  observeEvent(input$subcorp2, showModal(subcorpModal(2, force_include=TRUE)))
+  observeEvent(input$subcorp1, showModal(subcorpModal(1, globalAndResultsMeta(), force_include=TRUE)))
+  observeEvent(input$subcorp2, showModal(subcorpModal(2, globalAndResultsMeta(), force_include=TRUE)))
   observeEvent(input$refineSubcorpSelection1, showModal(refineSubcorpModal(1)))
   observeEvent(input$refineSubcorpSelection2, showModal(refineSubcorpModal(2)))
   observeEvent(input$okSubcorp1, confirmSubcorp(1))

genre_diff.R
@@ -2,7 +2,7 @@ library(ggplot2)
 library(dplyr)
 
 genreDiff <- function(data, ids1, ids2) {
-  data <- select(data, -X, -MODE, -DIVISION, -SUPERCLASS, -CLASS)
+  data <- select(data, -X, -MODE, -DIVISION, -DIVISION_ORIG, -SUPERCLASS, -CLASS)
   ncols <- ncol(data)
   results = data.frame(Factor=names(data),
                        Diff=rep(0, ncols),

load_data.R
@@ -12,16 +12,15 @@ loadData <- function(path) {
   orig <- env$res.data
 
   fdf <- env$factors
+  fdf$DIVISION_ORIG <- as.factor(fdf$DIVISION)
   # expand division label to make it more intuitive
   fdf$DIVISION <- paste(as.character(fdf$MODE), as.character(fdf$DIVISION), sep="-")
-  # coerce metadata to factors
-  fdf$MODE <- as.factor(fdf$MODE)
-  fdf$DIVISION <- as.factor(fdf$DIVISION)
+  # coerce metadata to factors; reorder MODE and DIVISION levels so that any
+  # additional non-Koditex data goes last to keep original visuals
+  fdf$MODE <- as.factor(fdf$MODE) %>% orig_levels_first()
+  fdf$DIVISION <- as.factor(fdf$DIVISION) %>% orig_levels_first()
   fdf$SUPERCLASS <- as.factor(fdf$SUPERCLASS)
   fdf$CLASS <- as.factor(fdf$CLASS)
-  # reorder levels so that cpact* goes last to keep original visuals
-  fdf$MODE <- orig_levels_first(fdf$MODE)
-  fdf$DIVISION <- orig_levels_first(fdf$DIVISION)
   fdf$X <- row.names(fdf)
   ffactors <- grep("^(X|MODE|DIVISION|SUPERCLASS|CLASS)$", colnames(fdf), value=TRUE, invert=TRUE)
   modes <- levels(fdf$MODE)

model_cmp.R
@@ -64,8 +64,8 @@ modelCmpFeatures <- function(ldf1, ldf2) {
 }
 
 modelCmpChunks <- function(fdf1, fdf2) {
-  fdf1 = select(fdf1, -MODE, -DIVISION, -SUPERCLASS, -CLASS, -X)
-  fdf2 = select(fdf2, -MODE, -DIVISION, -SUPERCLASS, -CLASS, -X)
+  fdf1 = select(fdf1, -MODE, -DIVISION, -DIVISION_ORIG, -SUPERCLASS, -CLASS, -X)
+  fdf2 = select(fdf2, -MODE, -DIVISION, -DIVISION_ORIG, -SUPERCLASS, -CLASS, -X)
   cormat = initCormat()
   for(f1 in colnames(fdf1)) {
     val1 = data.frame(Chunk = rownames(fdf1), load = fdf1[,f1])

commit 791cef3c0806553adc06f8ce0250c08925667100

Fix GenreDiff for Koditex data
dbb801c introduced GenreDiff support for non-Koditex data, but in the
process, it broke it for Koditex data, because this line...

  non_koditex_chunks$include <- "yes"

... doesn't work if non_koditex_chunks has 0 rows. Whereas this new
solution which extends the dplyr pipeline with mutate does.

server.R
@@ -308,11 +308,11 @@ function(input, output, session) {
     non_koditex_ids <- setdiff(rownames(fdf), globalMeta$id)
     non_koditex_chunks <- fdf[non_koditex_ids, ] %>%
       rownames_to_column("id") %>%
-      select(id, mode=MODE, division=DIVISION_ORIG)
-    # consider all additional chunks extracted from results as include=yes,
-    # otherwise the user would have to change this manually every time, because
-    # we want to keep force_include=TRUE for Koditex data
-    non_koditex_chunks$include <- "yes"
+      select(id, mode=MODE, division=DIVISION_ORIG) %>%
+      # consider all additional chunks extracted from results as include=yes,
+      # otherwise the user would have to change this manually every time, because
+      # we want to keep force_include=TRUE for Koditex data
+      mutate(include="yes")
     bind_rows(globalMeta, non_koditex_chunks) %>%
       replace(is.na(.), "-NA-")
   })